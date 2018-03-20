2c11f6e8bcd504a907b9b64d8fe45c67051f8a6c
Guest User <Guest@Epicodus-3E.local>
2018-03-20 11:09:03 -0700

    First commit API WORKS

---
+++ spec/food-face-spec.js
@@ -0,0 +1,9 @@
+// import { Constructor } from './../src/food-face.js'
+
+describe('', function() {
+
+  it('', function() {
+    // var example = new Constructor(parameters)
+    // expect(example.method()).toEqual('');
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from:'./src/img',to:'images'}
+    ]),
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'food-face',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i, // image code
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader', // image code
+            options: {
+              bypassOnDebug: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude:[
+          /node_modules/,
+          /spec/
+        ],
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader:'babel-loader',
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
